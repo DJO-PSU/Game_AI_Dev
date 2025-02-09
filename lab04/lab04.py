f9f82c846a60d972738162c96bd7c8c3c10e4ea5
DJO-PSU <djo5384@psu.edu>
2025-02-09 18:15:20 -0500

    lab4

---
+++ lab04/lab04.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).parents[1]))
+
+from util.llm_utils import TemplateChat
+
+def run_console_chat(sign, **kwargs):
+    chat = TemplateChat.from_file(sign=sign, **kwargs)
+    chat_generator = chat.start_chat()
+    print(next(chat_generator))
+    while True:
+        try:
+            message = chat_generator.send(input('You: '))
+            print('Agent:', message)
+        except StopIteration as e:
+            if isinstance(e.value, tuple):
+                print('Agent:', e.value[0])
+                ending_match = e.value[1]
+                print('Ending match:', ending_match)
+            break
+
+lab04_params = {
+    "template_file": "lab04\lab04_trader_chat.json",
+    "sign": "Devon",
+    "inventory": ["Mana Potion", "Health Potion"],
+    "end_regex": r"ORDER(.*)DONE"
+}
+
+if __name__ ==  '__main__':
+    # run lab04.py to test your template interactively
+    trader_template_file = 'lab04\lab04_trader_chat.json'
+    run_console_chat(template_file=trader_template_file,
+                   inventory='[Health Potion, Strength Potion]',
+                     sign='Devon',
+                     end_regex=r'ORDER(.*)DONE')
