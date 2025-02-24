7fca9b64d1c6b327b5ef7de65031bc1b23dddf20
DJO-PSU <djo5384@psu.edu>
2025-02-23 23:12:27 -0500

    lab05

---
+++ lab05/lab05.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parents[1]))
+
+import random
+from util.llm_utils import run_console_chat, tool_tracker
+
+# beauty of Python
+@tool_tracker
+def process_function_call(function_call):
+    print('Inside process_function_call')
+    name = function_call.name
+    args = function_call.arguments
+
+    return globals()[name](**args)
+
+def roll_for(skill, dc, player):
+    n_dice = 1
+    sides = 20
+    roll = sum([random.randint(1, sides) for _ in range(n_dice)])
+    if roll >= dc:
+        return f'{player} rolled {roll} for {skill} and succeeded!'
+    else:
+        return f'{player} rolled {roll} for {skill} and failed!'
+
+def process_response(self, response):
+    # Fill out this function to process the response from the LLM
+    
+    print('Inside process_response')
+    if response.message.tool_calls:
+        self.messages.append({'role': 'tool',
+                          'name': response.message.tool_calls[0].function.name, 
+                         'arguments': response.message.tool_calls[0].function.arguments,
+                          'content': process_function_call(response.message.tool_calls[0].function)
+                       })
+        #print(response)
+        return self.completion()
+    # ad make the function call 
+    return response
+#chat = TemplateChat.from_file('lab05/lab05_dice_template.json')
+
+run_console_chat(template_file='lab05/lab05_dice_template.json',
+                 process_response=process_response)
